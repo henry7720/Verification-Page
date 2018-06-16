d44ddfb51c8257c6f7e8ea5a5275d8430b6ca2d5
Henry <19752455+henry7720@users.noreply.github.com>
2018-06-16 17:12:53 -0500

    Update end-session.php

--- end-session.php
+++ end-session.php
@@ -2,5 +2,5 @@
 session_start();
 session_destroy();
 header("Location: /verification.php");
-# Make sure to make this redirect where you want it to, either back to the verification page, or the masked homepage; the verification page is important to redirect to so it doesn't have to redirect twice (unnecessary).
+# Make sure to make this redirect where you want it to, preferably the verification page.
 ?>
