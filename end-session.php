5a34f3cdc320e746babf5453359b7a086dffedf3
Henry <19752455+henry7720@users.noreply.github.com>
2018-07-06 22:45:39 -0500

    Update end-session.php

--- end-session.php
+++ end-session.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+session_unset();
 session_destroy();
 header("Location: /verification.php");
 # Make sure to make this redirect where you want it to, preferably the verification page.
