e785dd1f2f29351eea9886708aa0d4d5acc284b1
Henry <19752455+henry7720@users.noreply.github.com>
2018-09-04 17:18:21 -0500

    Create hashgenerator.php
    Hash generator for passwords.

---
+++ hashgenerator.php
@@ -0,0 +1,29 @@
+<?php
+if (isset($_POST["key-to-hash"])) {
+  $keytohash = trim($_POST["key-to-hash"]);
+  $hashedkey = password_hash($keytohash, PASSWORD_DEFAULT);
+  if (strlen($keytohash) >= 8 && strlen($keytohash) <= 32) {
+    $message = "Your BCRYPT hashed password is <span style=\"background-color:#ffff00;font-weight:bold\">$hashedkey</span> !";
+  } else {
+    $message = "Please enter in a password that is at least 8 characters, but no more than 32.";
+  }
+}
+?>
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title>BCRYPT Password Generator</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="stylesheet" href="verification.css">
+  </head>
+  <body>
+    <h1>BCRYPT Password Generator</h1>
+    <p>Please enter in a password to be hashed for use in the verification system.</p>
+    <form action="hashgenerator.php" method="post" autocomplete="off">
+      <input type="text" name="key-to-hash" id="key-to-hash" placeholder="Key">
+      <input type="submit" value="Hash">
+    </form>
+<?php if (isset($message)) echo "    <p>$message</p>\n"; ?>
+  </body>
+</html>
