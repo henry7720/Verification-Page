7303d794784681cfa4dab55ed350f2e6d0b1ba45
Henry <19752455+henry7720@users.noreply.github.com>
2018-09-08 11:07:39 -0500

    Update verification.php

--- verification.php
+++ verification.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$hashedkey = '$2y$10$xTYSKWUWnUo8dFcf.szEiO1J6nTfg7fOf1.cTx.X23II2sDVLpkC2';
+$hashedkey = '$2y$10$mWUUCN1B6NQkWQ1hiTbfD.xZ3Fs7rppU.A2OQm0LU2pWmsn9kVLHC';
 # Create a hash for a password with hashgenerator.php; in this case, I used "test1234"
 if (isset($_SESSION["verified"]) && $_SESSION["verified"]) {
   header("Location: /index.php");
@@ -8,9 +8,15 @@ if (isset($_SESSION["verified"]) && $_SESSION["verified"]) {
 }
 
 if (isset($_POST["key"])) {
-  $sha256hash = hash("sha256", trim($_POST["key"]));
+  $key = trim($_POST["key"]);
+  $verifiedpassword = password_verify(
+    base64_encode(
+      hash("sha256", $pass, true)
+    ),
+    $hashedkey
+  );
   # Sanitized input to make it easier the enter in the password; it is very easy to strengthen these restrictions, or lessen them.
-  if (password_verify($sha256hash, $hashedkey)) {
+  if ($verifiedpassword) {
     $_SESSION["verified"] = true;
     $whitelist = ["/index.php"];
     # Add any other pages you wish to be accessible through the continue param.
@@ -37,7 +43,7 @@ if (isset($_POST["key"])) {
     <h1>Verify to Continue</h1>
     <p>Please enter in the verification key to continue.</p>
     <form action="verification.php<?php if (isset($_GET["continue"])) echo "?continue=" . htmlentities($_GET["continue"]); ?>" method="post" autocomplete="off">
-      <input type="text" name="key" id="key" placeholder="Key">
+      <input type="password" name="key" id="key" placeholder="Key">
       <input type="submit" value="Verify">
     </form>
 <?php if (isset($error)) echo "    <p>$error</p>\n"; ?>
