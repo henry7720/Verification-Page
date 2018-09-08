6398c0bc6a20be2f8a675b1cc674cb41ac702c5b
Henry <19752455+henry7720@users.noreply.github.com>
2018-09-08 09:05:28 -0500

    Add extra layer of security with sha256

--- hashgenerator.php
+++ hashgenerator.php
@@ -1,7 +1,8 @@
 <?php
 if (isset($_POST["key-to-hash"])) {
   $keytohash = trim($_POST["key-to-hash"]);
-  $hashedkey = password_hash($keytohash, PASSWORD_DEFAULT);
+  $sha256hash = hash("sha256", $keytohash);
+  $hashedkey = password_hash($sha256hash, PASSWORD_DEFAULT);
   if (strlen($keytohash) >= 8 && strlen($keytohash) <= 32) {
     $message = "Your BCRYPT hashed password is <span style=\"background-color:#ffff00;font-weight:bold\">$hashedkey</span> !";
   } else {
@@ -21,7 +22,7 @@ if (isset($_POST["key-to-hash"])) {
     <h1>BCRYPT Hash Generator</h1>
     <p>Please enter in a password to be hashed for use in the verification system.</p>
     <form action="hashgenerator.php" method="post" autocomplete="off">
-      <input type="text" name="key-to-hash" id="key-to-hash" placeholder="Key">
+      <input type="text" name="key-to-hash" id="key-to-hash" maxlength="32" placeholder="Key">
       <input type="submit" value="Hash">
     </form>
 <?php if (isset($message)) echo "    <p>$message</p>\n"; ?>
